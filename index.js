57b3c9a419ad7b3f9c0154ae7f7bc1a5671a5c7f
DeroMal <derrickmal123@gmail.com>
2023-09-26 20:09:17 +0200

    feat: Enhance File Renamer UI and add auto-delete functionality
    - Redesigned the frontend for a more appealing and user-friendly experience.
    - Added animations and visual feedback elements.
    - Implemented auto-deletion of user files after 1 hour for efficient storage management.
    - Added clear user instructions and notifications on the frontend.

--- index.js
+++ index.js
@@ -1,86 +1,121 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-const Papa = require('papaparse');
-const archiver = require('archiver');
-const uuid = require('uuid');
+const express = require('express')
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
+const Papa = require('papaparse')
+const archiver = require('archiver')
+const uuid = require('uuid')
 
-const app = express();
-const port = 3000;
 
-// Configuration Constants
-const UPLOAD_DELAY = 60 * 1000;  // 60 seconds
-const PDF_UPLOAD_LIMIT = 64;
-
-// Multer setup for file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-// Serve static files from the "public" directory
-app.use(express.static('public'));
-
-// Schedule a deletion of a folder after a delay
-function scheduleDeletion(dirPath, delay) {
-    setTimeout(() => {
-        if (fs.existsSync(dirPath)) {
-            fs.rmSync(dirPath, { recursive: true, force: true });
-        }
-    }, delay);
-}
-
-app.post('/upload', upload.fields([{ name: 'csv' }, { name: 'pdfs', maxCount: PDF_UPLOAD_LIMIT }]), (req, res) => {
-    try {
-        const csvFile = req.files.csv[0].buffer.toString('utf-8');
-        const pdfFiles = req.files.pdfs;
-        
-        const orgNames = [];
-        Papa.parse(csvFile, {
-            header: true,
-            skipEmptyLines: true,
-            complete: (results) => {
-                results.data.forEach(row => orgNames.push('-' + row.organization));
+function deleteFolderRecursive(directoryPath) {
+    if (fs.existsSync(directoryPath)) {
+        fs.readdirSync(directoryPath).forEach((file, index) => {
+            const curPath = path.join(directoryPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
             }
         });
-
-        const uniqueFolder = uuid.v4();
-        const tempDir = path.join(__dirname, 'uploads', uniqueFolder);
-        fs.mkdirSync(tempDir);
-
-        const output = fs.createWriteStream(path.join(tempDir, 'renamed_pdfs.zip'));
-        const archive = archiver('zip');
-        output.on('close', () => {
-            return res.json({
-                message: "All files have been successfully renamed!",
-                downloadLink: `/uploads/${uniqueFolder}/renamed_pdfs.zip`
-            });
-            scheduleDeletion(tempDir, UPLOAD_DELAY);
-        });
-
-        archive.pipe(output);
-        pdfFiles.forEach((pdfFile, idx) => {
-            const newName = pdfFile.originalname.replace(` ${idx + 1}.pdf`, ` ${orgNames[idx]}.pdf`);
-            const newPath = path.join(tempDir, newName);
-            fs.writeFileSync(newPath, pdfFile.buffer);
-            archive.append(fs.createReadStream(newPath), { name: newName });
-        });
-
-        archive.finalize();
-    } catch (error) {
-        res.status(500).json({ message: "An error occurred during processing. Please try again." });
+        fs.rmdirSync(directoryPath);
     }
-});
+}
 
-app.get('/uploads/:folder/renamed_pdfs.zip', (req, res, next) => {
-    const folder = req.params.folder;
-    const zipPath = path.join(__dirname, 'uploads', folder, 'renamed_pdfs.zip');
-    
-    if (fs.existsSync(zipPath)) {
-        scheduleDeletion(path.join(__dirname, 'uploads', folder), UPLOAD_DELAY);
-    } 
-    next();
-});
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+app.post(
+  '/upload',
+  upload.fields([{ name: 'csv' }, { name: 'pdfs' }]),
+  (req, res) => {
+    // Check if uploaded files are present
+    if (!req.files.csv || !req.files.csv[0] || !req.files.pdfs) {
+      return res
+        .status(400)
+        .json({ message: 'Please upload both CSV and PDF files.' })
+    }
+
+    const csvFile = req.files.csv[0].buffer.toString('utf-8')
+    const pdfFiles = req.files.pdfs
+
+    // Validate file types
+    if (req.files.csv[0].mimetype !== 'text/csv') {
+      return res
+        .status(400)
+        .json({ message: 'Please upload a valid CSV file.' })
+    }
+    for (let pdfFile of req.files.pdfs) {
+      if (!pdfFile.mimetype.startsWith('application/pdf')) {
+        return res
+          .status(400)
+          .json({ message: 'Please upload only PDF files.' })
+      }
+    }
+
+    const orgNames = [];
+    Papa.parse(csvFile, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results) => {
+            results.data.forEach(row => orgNames.push(row.organization));
+        }
+    });
+
+    const uniqueFolder = uuid.v4()
+
+    // Schedule the deletion of the folder after 1 hour
+    setTimeout(() => {
+        deleteFolderRecursive(tempDir);
+    }, 60000); // 1 hour in milliseconds
+
+    const tempDir = path.join(__dirname, 'uploads', uniqueFolder)
+    fs.mkdirSync(tempDir)
+
+    const output = fs.createWriteStream(path.join(tempDir, 'renamed_pdfs.zip'))
+    const archive = archiver('zip')
+    output.on('close', () => {
+      return res.json({
+        message: 'All files have been successfully renamed!',
+        downloadLink: `/uploads/${uniqueFolder}/renamed_pdfs.zip`
+      })
+    })
+
+    archive.pipe(output)
+    pdfFiles.forEach((pdfFile, idx) => {
+        const newName = pdfFile.originalname.replace(`${idx + 1}.pdf`, `${orgNames[idx]}.pdf`);
+
+      const newPath = path.join(tempDir, newName)
+      fs.writeFileSync(newPath, pdfFile.buffer)
+      archive.append(fs.createReadStream(newPath), { name: newName })
+    })
+
+    archive.finalize()
+  }
+)
+
+app.get('/uploads/:folder/renamed_pdfs.zip', (req, res) => {
+  const folder = req.params.folder
+  const zipPath = path.join(__dirname, 'uploads', folder, 'renamed_pdfs.zip')
+
+  if (fs.existsSync(zipPath)) {
+    res.setHeader('Content-Type', 'application/zip')
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename=renamed_pdfs.zip'
+    )
+    const fileStream = fs.createReadStream(zipPath)
+    fileStream.pipe(res)
+  } else {
+    res.status(404).send('File not found')
+  }
+})
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
