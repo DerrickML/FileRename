d5d9a02e7e5ec22cdf54e590d39144d3c7b7d23f
DeroMal <derrickmal123@gmail.com>
2023-09-26 17:07:41 +0300

    index.js changes: improved error handling, contants & configuration, logging, comments added, functions refactoring

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const fs = require('fs');
 const path = require('path');
 const Papa = require('papaparse');
 const archiver = require('archiver');
-const uuid = require('uuid'); // to generate unique folders
+const uuid = require('uuid');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// Configuration Constants
+const UPLOAD_DELAY = 60 * 1000;  // 60 seconds
+const PDF_UPLOAD_LIMIT = 64;
 
+// Multer setup for file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Schedule a deletion of a folder after a delay
 function scheduleDeletion(dirPath, delay) {
     setTimeout(() => {
         if (fs.existsSync(dirPath)) {
@@ -22,42 +29,46 @@ function scheduleDeletion(dirPath, delay) {
     }, delay);
 }
 
-app.post('/upload', upload.fields([{ name: 'csv' }, { name: 'pdfs', maxCount: 64 }]), (req, res) => {
-    const csvFile = req.files.csv[0].buffer.toString('utf-8');
-    const pdfFiles = req.files.pdfs;
-    
-    const orgNames = [];
-    Papa.parse(csvFile, {
-        header: true,
-        skipEmptyLines: true,
-        complete: (results) => {
-            results.data.forEach(row => orgNames.push('-' + row.organization));
-        }
-    });
-
-    const uniqueFolder = uuid.v4();
-    const tempDir = path.join(__dirname, 'uploads', uniqueFolder);
-    fs.mkdirSync(tempDir);
-
-    const output = fs.createWriteStream(path.join(tempDir, 'renamed_pdfs.zip'));
-    const archive = archiver('zip');
-    output.on('close', () => {
-        return res.json({
-            message: "All files have been successfully renamed!",
-            downloadLink: `/uploads/${uniqueFolder}/renamed_pdfs.zip`
+app.post('/upload', upload.fields([{ name: 'csv' }, { name: 'pdfs', maxCount: PDF_UPLOAD_LIMIT }]), (req, res) => {
+    try {
+        const csvFile = req.files.csv[0].buffer.toString('utf-8');
+        const pdfFiles = req.files.pdfs;
+        
+        const orgNames = [];
+        Papa.parse(csvFile, {
+            header: true,
+            skipEmptyLines: true,
+            complete: (results) => {
+                results.data.forEach(row => orgNames.push('-' + row.organization));
+            }
         });
-        scheduleDeletion(tempDir, 60 * 1000);
-    });
 
-    archive.pipe(output);
-    pdfFiles.forEach((pdfFile, idx) => {
-        const newName = pdfFile.originalname.replace(` ${idx + 1}.pdf`, ` ${orgNames[idx]}.pdf`);
-        const newPath = path.join(tempDir, newName);
-        fs.writeFileSync(newPath, pdfFile.buffer);
-        archive.append(fs.createReadStream(newPath), { name: newName });
-    });
+        const uniqueFolder = uuid.v4();
+        const tempDir = path.join(__dirname, 'uploads', uniqueFolder);
+        fs.mkdirSync(tempDir);
 
-    archive.finalize();
+        const output = fs.createWriteStream(path.join(tempDir, 'renamed_pdfs.zip'));
+        const archive = archiver('zip');
+        output.on('close', () => {
+            return res.json({
+                message: "All files have been successfully renamed!",
+                downloadLink: `/uploads/${uniqueFolder}/renamed_pdfs.zip`
+            });
+            scheduleDeletion(tempDir, UPLOAD_DELAY);
+        });
+
+        archive.pipe(output);
+        pdfFiles.forEach((pdfFile, idx) => {
+            const newName = pdfFile.originalname.replace(` ${idx + 1}.pdf`, ` ${orgNames[idx]}.pdf`);
+            const newPath = path.join(tempDir, newName);
+            fs.writeFileSync(newPath, pdfFile.buffer);
+            archive.append(fs.createReadStream(newPath), { name: newName });
+        });
+
+        archive.finalize();
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred during processing. Please try again." });
+    }
 });
 
 app.get('/uploads/:folder/renamed_pdfs.zip', (req, res, next) => {
@@ -65,7 +76,7 @@ app.get('/uploads/:folder/renamed_pdfs.zip', (req, res, next) => {
     const zipPath = path.join(__dirname, 'uploads', folder, 'renamed_pdfs.zip');
     
     if (fs.existsSync(zipPath)) {
-        scheduleDeletion(path.join(__dirname, 'uploads', folder), 60 * 1000);
+        scheduleDeletion(path.join(__dirname, 'uploads', folder), UPLOAD_DELAY);
     } 
     next();
 });
